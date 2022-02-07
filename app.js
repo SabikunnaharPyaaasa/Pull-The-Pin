9e1126686ff053bff735b381eba2ad287135a157
Sabikinnaha Pyaasa <55408189+SabikunnaharPyaaasa@users.noreply.github.com>
2022-02-07 16:04:46 +0600

    Add files via upload

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 var vid = document.getElementById("myVideo");
 var i = 1.5;
 var j = 1;
-vid.playbackRate = 0.8;
+vid.playbackRate = 1.2;
 icon1  = document.getElementById('icon1');
 icon2  = document.getElementById('icon2');
 icon3  = document.getElementById('icon3');
+play_btn  = document.getElementById('play_btn');
 
 
 function playVid()
@@ -25,21 +26,23 @@ function pauseVid(){
     vid.pause();
     j=j+1;
     if(j==2){
-        icon2.style="display:block; border-radius:5px; font-size:48px; color:black"
+        icon2.style="display:block; font-size:48px; color:black"
 }
 else if(j==3){
-        icon3.style="display:block; border-radius:20px; font-size:40px; color:black"
+        icon3.style="display:block;  font-size:40px; color:black"
 }
     i=i+2.1;
     if(i>7.5)
     {
        
         showLogo();
+        play_btn.style="display:block; border-radius:50px; font-size:40px; color:black"
     }
 }
 
 function showLogo() {
     document.getElementById('game_screen').innerHTML = "<img src =logo.png width=\"300px\" height=\"400px\">";
+ 
   }
 
 // function showLogo(){
